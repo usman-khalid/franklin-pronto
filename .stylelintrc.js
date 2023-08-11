e82dc7e5530c205c0012d5c41be8e2beb856f43b
Usman Khalid <usman.khalid@live.ca>
2023-08-10 22:16:22 -0400

    chore: remove lint build, consolidate with franklin build

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,6 +4,10 @@ module.exports = {
     "shorthand-property-no-redundant-values": true,
     "declaration-block-no-duplicate-properties": true,
     "declaration-block-no-redundant-longhand-properties": true,
-    "no-unknown-animations": true
+    "no-unknown-animations": true,
+    "at-rule-no-unknown": [true, {
+      "ignoreAtRules": ["mixin"]
+    }],
+    "import-notation": null,
   },
 };
