bdbdc420fbd179e5f3d6393d92cbde9cdf627c95
Usman Khalid <usman.khalid@live.ca>
2023-12-15 14:46:40 -0500

    chore: improve global selector for hero, remove dist for workflow testing

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,6 +1,5 @@
 import heroClasses from './hero.module.css';
 
 export default async function decorate(block) {
-  block.closest('.section').classList.add(heroClasses.heroContainer);
   block.classList.add(heroClasses.hero);
 }
